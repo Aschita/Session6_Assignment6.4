2b7b9e123bfaa95eb5d1cdea8affd065cb1396f5
Aschita <aschita02@gmail.com>
2017-07-03 12:10:55 +0530

    Add files via upload

---
+++ BankAtmException.java
@@ -0,0 +1,18 @@
+package assignment_6d;
+
+
+	public class BankAtmException extends Exception {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		//Class declaration which is the child class of inbuilt class Exception.  
+		
+			public String toString()         //function to return string of Exception.
+			{
+				return "Generating BankATMException because either balance is less than 10000 or withdrawl amount is greater than balance available.";
+						//returning String denoting that the Exception is happened.
+			}
+			
+	}
